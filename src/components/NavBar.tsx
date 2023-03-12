c03fe79d927baf2a50aaea3cf2ee4e2e954600fc
krishanb4 <krishanbandara4444@gmail.com>
2023-03-12 21:41:08 +0530

    final touch

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,34 @@
 import { Web3Button } from "@web3modal/react";
 import React from "react";
+import Image from "next/image";
 
 const Navbar: React.FC = () => {
   return (
     <>
       {/* Desktop + Large Desktop */}
       <nav className="w-9/12 mx-auto hidden lg:flex flex-row items-center pt-6 xl:pt-12 justify-between">
-        <img src="/logo.png" className="h-10 xl:h-12" />
+        <Image
+          src="/logo.png"
+          className="h-21 xl:h-21"
+          alt=""
+          width="400"
+          height="100"
+        />
         {/* <button className="w-40 xl:w-48 py-2 xl:py-3 px-3 z-20 flex flex-row items-center justify-center rounded-lg cursor-pointer bg-blue-600 hover:bg-blue-700">
       
           <p className="text-lg xl:text-xl font-medium text-white">Connect</p>
         </button> */}
-        <Web3Button/>
+        <Web3Button />
       </nav>
       {/* Mobile + Tablet */}
       <nav className="w-10/12 mx-auto flex flex-row lg:hidden pt-8 justify-between">
-        <img src="/logo.png" className="h-8" />
+        <Image
+          src="/logo.png"
+          className="h-21"
+          alt=""
+          width="400"
+          height="100"
+        />
         {/* <img
           className="w-6 h-6"
           src={
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/NavBar";
 import Head from "next/head";
+import { Web3Button } from "@web3modal/react";
+import Image from "next/image";
 const IndexPage = () => {
   return (
     <>
@@ -8,10 +10,10 @@ const IndexPage = () => {
           name="viewport"
           content="width=device-width, initial-scale=1 maximum-scale=1"
         />
-        <title>IgnoreFud Airdrop Cliam</title>
+        <title>IgnoreFud Airdrop Claim</title>
       </Head>
       {/* Desktop + Large Desktop */}
-      <div className="hidden lg:flex flex-col w-full h-screen bg-primaryLight">
+      <div className="hidden lg:flex flex-col w-full h-screen bg-[url('../../public/images/body-bg.png')]">
         <Navbar />
         <div className="w-9/12 mx-auto mt-16 2xl:mt-40">
           <h1 className="text-6xl xl:text-8xl font-light w-5/12 xl:w-1/2 2xl:w-4/12">
@@ -23,29 +25,34 @@ const IndexPage = () => {
             <span className="font-medium">Metamask wallet</span>.
           </p>
         </div>
-        <img
-          src="/images/Mint blue cat hands up.svg"
+        <Image
+          src="/images/airdrop.svg"
           alt="catHandsUp"
           className="absolute right-28 xl:right-40 bottom-60 xl:bottom-60 2xl:bottom-80 z-10 w-5/12"
+          width={300}
+          height={300}
         />
-        <img
+        {/* <img
           src="/images/Mint blue cat with umbrella.svg"
           alt="catUmbrella"
           className="absolute bottom-20 right-48 xl:right-60 2xl:right-112 z-10 w-4/12 2xl:w-4/12"
-        />
-        <img
+        /> */}
+        {/* <img
           src="/images/Yellow_cat.png"
           alt="catYellow"
           className="absolute bottom-20 2xl:bottom-80 left-20 xl:left-40 z-10 w-5/12 2xl:w-4/12"
-        />
-        <img
+        /> */}
+
+        <Image
           src="/images/Background_ hill.svg"
           alt="bgHill"
-          className="absolute bottom-0 w-full"
+          width="300"
+          height="300"
+          className="absolute bottom-0 w-full fill-[#bce4af]"
         />
       </div>
       {/* Mobile + Tablet */}
-      <div className="w-full h-screen lg:hidden bg-primaryLight">
+      <div className="w-full h-screen lg:hidden bg-[url('../../public/images/body-bg.png')]">
         <Navbar />
         <div className="relative w-10/12 text-center mx-auto mt-12 md:mt-40 lg:mt-56 z-20">
           <h1 className="text-5xl font-light">Claim your Airdrop Now!</h1>
@@ -60,24 +67,26 @@ const IndexPage = () => {
           >
             <p className="text-xl font-medium text-white">Connect Wallet</p>
           </button> */}
-          <button className="relative w-48 py-3 px-3 mt-4 z-40 mx-auto flex flex-row items-center justify-center rounded-xl cursor-pointer bg-blue-600 hover:bg-blue-700">
-            <p className="text-xl font-medium text-white">Connect Wallet</p>
-          </button>
+          <Web3Button />
         </div>
-        <img
-          src="/images/mobileBlueCat.svg"
+        <Image
+          src="/images/airdrop.svg"
           alt="catHandsUp"
           className="absolute right-0 bottom-5 md:bottom-20 md:right-5 lg:right-20 z-10 md:w-4/12 lg:w-3/12"
+          width={300}
+          height={300}
         />
-        <img
+        {/* <img
           src="/images/Yellow_cat.png"
           alt="catYellow"
           className="absolute bottom-16 md:bottom-36 left-0 lg:left-18 w-8/12 lg:w-6/12 z-10"
-        />
-        <img
+        /> */}
+        <Image
           src="/images/mobileBg.svg"
           alt="bgHill"
           className="absolute bottom-0 w-full"
+          width={300}
+          height={300}
         />
       </div>
     </>
